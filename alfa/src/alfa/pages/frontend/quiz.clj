16a6db0fb58ece7cba93d981603bf8bc3bfcbb38
aisyahgunung <aisyah.gunung@zeniuseducation.com>
2015-11-27 00:49:09 +0700

    quiz

--- alfa/src/alfa/pages/frontend/quiz.clj
+++ alfa/src/alfa/pages/frontend/quiz.clj
@@ -5,15 +5,16 @@
     [alfa.pages.frontend.components :as components]))
 
 (def prob-image
-  [:div {:class "widget clearfix", :id "flickr"}
-   [:div {:data-lightbox "gallery", :data-type "group", :data-count "16", :data-id "613394@N22", :class "flickr-feed masonry-thumbs", :id "flickr-widget"}]])
+  [:div {:class "entry-image"}
+   [:a {:href "#"}
+    [:img {:alt "Image", :src "images/Banana.jpg"}]]])
 
 (def prob-question
   [:div {:class "widget subscribe-widget clearfix", :id "subscriber"}
    [:h5 "This is where you ask the questions. And below are the options. For example, what is the colour of your nail?"]])
 
 (def prob-option
-  [:ul {:style "max-width: 300px;", :class "nav nav-pills nav-stacked"}
+  [:ul {:class "nav nav-pills nav-stacked"}
    [:li [:a {:href "#"} "Red"]]
    [:li [:a {:href "#"} "Blue"]]
    [:li [:a {:href "#"} "Yellow"]]
@@ -23,23 +24,23 @@
 (def problem
   [:div {:class "widget clearfix", :id "text"}
    [:h3 {:class "highlight-me"} "Title of Problem"]
-   prob-image
-   prob-question
-   prob-option])
+   [:div {:class "col-md-12"}
+    [:div {:class "col-md-5"} prob-image]
+    [:div {:class "col-md-7"} prob-question]]
+   [:div {:class "col-md-12"} prob-option]])
 
 (def social
-  [:div {:class "line"}
-   [:div {:class "widget quick-contact-widget clearfix", :id "s-icons"}
-    [:a {:title "Facebook", :class "social-icon si-colored si-facebook", :href "#"}
-     [:i {:class "icon-facebook"}] [:i {:class "icon-facebook"}]]
-    [:a {:title "Twitter", :class "social-icon si-colored si-twitter", :href "#"}
-     [:i {:class "icon-twitter"}] [:i {:class "icon-twitter"}]]
-    [:a {:title "Github", :class "social-icon si-colored si-github", :href "#"}
-     [:i {:class "icon-github"}] [:i {:class "icon-github"}]]
-    [:a {:title "Google Plus", :class "social-icon si-colored si-gplus", :href "#"}
-     [:i {:class "icon-gplus"}] [:i {:class "icon-gplus"}]]
-    [:a {:title "Instagram", :class "social-icon si-colored si-instagram", :href "#"}
-     [:i {:class "icon-instagram"}] [:i {:class "icon-instagram"}]]]])
+  [:div {:class "widget quick-contact-widget clearfix", :id "s-icons"}
+   [:a {:title "Facebook", :class "social-icon si-colored si-facebook", :href "#"}
+    [:i {:class "icon-facebook"}] [:i {:class "icon-facebook"}]]
+   [:a {:title "Twitter", :class "social-icon si-colored si-twitter", :href "#"}
+    [:i {:class "icon-twitter"}] [:i {:class "icon-twitter"}]]
+   [:a {:title "Github", :class "social-icon si-colored si-github", :href "#"}
+    [:i {:class "icon-github"}] [:i {:class "icon-github"}]]
+   [:a {:title "Google Plus", :class "social-icon si-colored si-gplus", :href "#"}
+    [:i {:class "icon-gplus"}] [:i {:class "icon-gplus"}]]
+   [:a {:title "Instagram", :class "social-icon si-colored si-instagram", :href "#"}
+    [:i {:class "icon-instagram"}] [:i {:class "icon-instagram"}]]])
 
 (defn do-quiz
   ([] (hp/html5
@@ -63,20 +64,20 @@
               [:ul
                [:li [:a {:href "problems.html"}
                  [:div "Math"]]]
-               [:li [:a {:href ""}
+               [:li [:a {:href "#"}
                  [:div "Physics"]]]
-               [:li [:a {:href ""}
+               [:li [:a {:href "#"}
                  [:div "Biology"]]]
-               [:li [:a {:href ""}
+               [:li [:a {:href "#"}
                  [:div "English"]]]
-               [:li [:a {:href ""}
+               [:li [:a {:href "#"}
                  [:div "History"]]]]]]
 
             [:div {:class "col-md-9 bottommargin"}
-             [:div {:class "col-md-10 bottommargin"}
+             [:div {:class "col-md-11 bottommargin"}
               problem]
-             [:div {:class "col-md-2 bottommargin"}
-              social]]]]]
+             [:div {:class "col-md-1 bottommargin"}
+              [:br social]]]]]]
 
 
 
