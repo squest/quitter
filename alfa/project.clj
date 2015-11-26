96faa7b6731c9c8f2ae94206c1c4d72f7703d014
Peter Lunando <peter.lunando@zeniuseducation.com>
2015-11-26 18:58:54 +0700

    adding development source path

--- alfa/project.clj
+++ alfa/project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.stuartsierra/component "0.3.0"]
                  [io.pedestal/pedestal.service "0.4.0"]
@@ -13,4 +13,5 @@
                  ;;html
                  [hiccup "1.0.5"]
                  [selmer "0.9.2"]
-                 [hickory "0.5.4"]])
+                 [hickory "0.5.4"]]
+  :profiles {:dev {:source-paths ["dev"]}})
