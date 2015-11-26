592bdcbe1a39bccb308551fe8ce872f67d069494
Peter Lunando <peter.lunando@zeniuseducation.com>
2015-11-26 19:22:36 +0700

    adding dummy database component

--- alfa/src/alfa/databases/core.clj
+++ alfa/src/alfa/databases/core.clj
@@ -1,3 +1,16 @@
-(ns alfa.databases.core)
+(ns alfa.databases.core
+  (:require [com.stuartsierra.component :as component]))
 
 ;;Feel free to change things
+
+(declare make)
+
+(defrecord Database [db-uri part]
+  component/Lifecycle
+  (start [this])
+  (stop [this]))
+
+(defn make
+  [db-uri part mode]
+  (map->Database {:db-uri db-uri
+                  :part   part}))
