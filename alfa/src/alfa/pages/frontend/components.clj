3214989e32b4b054ed04043559db6cbf485e1e27
aisyahgunung <aisyah.gunung@zeniuseducation.com>
2015-11-26 19:28:56 +0700

    FE ns

---
+++ alfa/src/alfa/pages/frontend/components.clj
@@ -0,0 +1,4 @@
+(ns alfa.pages.frontend.components
+  (:require
+    [hiccup.core :as hc]
+    [hiccup.page :as hp]))
---
+++ alfa/src/alfa/pages/frontend/probleminput.clj
@@ -0,0 +1,5 @@
+(ns alfa.pages.frontend.probleminput
+  (:require
+    [hiccup.core :as hc]
+    [hiccup.page :as hp]
+    [alfa.pages.frontend.components :as components]))
