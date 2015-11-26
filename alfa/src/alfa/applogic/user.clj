3f23a312c9ff16e228e2f4a462a0f98ec5cd2271
Peter Lunando <peter.lunando@zeniuseducation.com>
2015-11-26 20:47:42 +0700

    user logic

---
+++ alfa/src/alfa/applogic/user.clj
@@ -0,0 +1,17 @@
+(ns alfa.applogic.user)
+
+(defn add-user!
+  "Add user... duh!!"
+  [])
+
+(defn set-user!
+  "To change user profile"
+  [])
+
+(defn set-password!
+  "To change user password"
+  [])
+
+(defn valid-user?
+  "Probably for login stuff"
+  [])
